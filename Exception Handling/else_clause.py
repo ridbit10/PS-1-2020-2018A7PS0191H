ba6a2deaca021494cb0c42c9d21b799b2f033a65
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-05-26 10:59:53 +0530

    Create else_clause.py

---
+++ Exception Handling/else_clause.py
@@ -0,0 +1,27 @@
+import sys
+
+Lst =['a',0,2,3]
+for i in Lst:
+    r=0
+    try:
+        print("Entry is ",i)
+        r=(1/int(i))
+        pass
+    except ValueError:
+        print ((sys.exc_info()[0]),"occured")
+        print("please enter a numerical value")
+        print()
+        pass
+    except (ZeroDivisionError): #can enter multiple error
+        print ((sys.exc_info()[0]),"occured")
+        print("please enter a non-zero value")
+        print()
+        pass
+    except:
+        print ((sys.exc_info()[0]),"occured")
+        print("please enter a non-zero value")
+        print()
+        pass
+    else:
+        print("reciprocal is",r)
+        print()
