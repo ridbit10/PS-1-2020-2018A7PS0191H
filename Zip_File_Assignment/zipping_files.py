d1ce3d1853cc40974e239a54c09bbebca0038fb5
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-01 12:41:24 +0530

    Create zipping_files.py

---
+++ Zip_File_Assignment/zipping_files.py
@@ -0,0 +1,27 @@
+from zipfile import ZipFile
+import os
+
+def file_paths(directory):
+    file_paths=[]
+    for root,directories,files in os.walk(directory):
+        for filename in files:
+            filepath=os.path.join(root,filename)
+            file_paths.append(filepath)
+
+    return file_paths
+try:
+    directory=input()         
+    file_pths=file_paths(directory)
+    
+    print("files to be ziiped")
+    for file in file_pths:
+        print(file)
+        
+    with ZipFile('my_python_files.zip','w') as zip:
+        for file in file_pths:
+            zip.write(file)
+
+except:
+    print ((sys.exc_info()[0]),"occured")
+else:
+    print("Files ziped")       
