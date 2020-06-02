00e2742499d995726723766ff3f39b97e03278b2
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-02 12:44:53 +0530

    Update pb.py

--- PhoneBook_Assignment/Protoype(Persistence)/pb.py
+++ PhoneBook_Assignment/Protoype(Persistence)/pb.py
@@ -33,59 +33,61 @@ def check_number(number):
 def add(contact):
     if(check_existence(contact.name)):
         print("contact already exists")
+        return 
     elif(check_number(contact.number)==0):
         print("number not correct")
+        return
+    try:
+        phone_book=open("phonebook.txt",'a',encoding='utf-8')
+        phone_book.write("{}:{}:{}\n".format(contact.name,contact.number,contact.address))
+        dict_phone[contact.name]=contact
+    except:
+        print ((sys.exc_info()[0]),"occuredb")
     else:
-        try:
-            phone_book=open("phonebook.txt",'a',encoding='utf-8')
-            phone_book.write("{}:{}:{}\n".format(contact.name,contact.number,contact.address))
-            dict_phone[contact.name]=contact
-        except:
-            print ((sys.exc_info()[0]),"occuredb")
-        else:
-            print("Contact succesfully added!!!!!")
-        finally:
-            phone_book.close()
+        print("Contact succesfully added!!!!!")
+    finally:
+        phone_book.close()
 
 def update(contact):
     if(check_existence(contact.name)==0):
         print("contact doesnot exist")
+        return
     elif(check_number(contact.number)==0):
         print("number not correct")
+        return
+    try:
+        dict_phone[contact.name]=contact
+        phone_book=open("phonebook.txt",'w',encoding='utf-8')
+        for name1 in dict_phone:
+                phone_book.write("{}:{}:{}\n".format(dict_phone[name1].name,dict_phone[name1].number,dict_phone[name1].address))
+    except:
+        print ((sys.exc_info()[0]),"occured")
     else:
-        try:
-            dict_phone[contact.name]=contact
-            phone_book=open("phonebook.txt",'w',encoding='utf-8')
-            for name1 in dict_phone:
-                    phone_book.write("{}:{}:{}\n".format(dict_phone[name1].name,dict_phone[name1].number,dict_phone[name1].address))
-        except:
-            print ((sys.exc_info()[0]),"occured")
-        else:
-            print("Contact succesfully updated!!!!!")
-        finally:
-            phone_book.close()
+        print("Contact succesfully updated!!!!!")
+    finally:
+        phone_book.close()
 
 def display(name1):
     if(check_existence(name1)==0):
         print("contact doesnot exist")
-    else:
-        print("{}:{}:{}".format(dict_phone[name1].name,dict_phone[name1].number,dict_phone[name1].address))
+        return
+    print("{}:{}:{}".format(dict_phone[name1].name,dict_phone[name1].number,dict_phone[name1].address))
 
 def delete(name1):
     if(check_existence(name1)==0):
         print("contact doesnot exist")
+        return
+    try:
+        phone_book=open("phonebook.txt",'w',encoding='utf-8')
+        del dict_phone[name1]
+        for name2 in dict_phone:
+            phone_book.write("{}:{}:{}\n".format(dict_phone[name2].name,dict_phone[name2].number,dict_phone[name2].address))
+    except:
+        print ((sys.exc_info()[0]),"occured")
     else:
-        try:
-            phone_book=open("phonebook.txt",'w',encoding='utf-8')
-            del dict_phone[name1]
-            for name2 in dict_phone:
-                phone_book.write("{}:{}:{}\n".format(dict_phone[name2].name,dict_phone[name2].number,dict_phone[name2].address))
-        except:
-            print ((sys.exc_info()[0]),"occured")
-        else:
-            print("Contact succesfully deleted!!!!!")
-        finally:
-            phone_book.close()
+        print("Contact succesfully deleted!!!!!")
+    finally:
+        phone_book.close()
 
 def specific_alphabet(alphabet):
     try:
