b1f6a92bbf34b39459ccb69a39a3fefd14113094
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-03 15:04:52 +0530

    Create company.py

---
+++ Student_internship/company.py
@@ -0,0 +1,7 @@
+class Company(object):
+    def __init__(self,name,field,type,CEO)
+            self.name=name
+            self.field=field
+            self.type=type
+            self.CEO=CEO
+    
