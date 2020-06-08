189fc0f18f09d696a1891912d37cc68c2ad12370
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-08 16:00:24 +0530

    Create sort_function.py

---
+++ Numpy/sort_function.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a=np.array(10)
+b=np.array([10,3,6,1,7,4])
+c=np.array([[4,2,3],[8,5,6]])
+d=np.array([[[1,2,9],[3,4,10]],[[5,6,11],[7,8,12]]])
+
+print(np.sort(b))
+
+s=np.array(['messi','neymar','alba','andres','mess'])
+print(np.sort(s))
+
+bo=np.array([True,False,True])
+print(np.sort(bo))
+
+print(np.sort(c))
