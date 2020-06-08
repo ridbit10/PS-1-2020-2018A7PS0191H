49eadb744a6d8924dd8dcc3fa4db0f4c784388f5
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-08 16:34:52 +0530

    Create ufuncs.py

---
+++ Numpy/ufuncs.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a=np.array([1,3,5,6])
+b=np.array([2,4,6,3])
+
+c=np.add(a,b)
+print(c)
+
+c=np.multiply(a,b)
+print(c)
+
+c=np.subtract(a,b)
+print(c)
+
+c=np.divide(a,b)
+print(c)
