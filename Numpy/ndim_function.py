c946796562e7cb4141495b8b8781db8d09ced037
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-08 13:54:31 +0530

    number of directoin in array

---
+++ Numpy/ndim_function.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a=np.array(10)
+b=np.array([1,2,3])
+c=np.array([[1,2],[1,2]])
+d=np.array([[[1,2],[1,2]],[[1,2],[1,2]]])
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+print(type(a))
