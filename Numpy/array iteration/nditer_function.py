850fb10ed94b2c037f6f18b2a1825d46960cbc74
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-08 15:06:16 +0530

    Create nditer_function.py

---
+++ Numpy/array iteration/nditer_function.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+a=np.array(10)
+b=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+c=np.array([[1,2,3],[4,5,6]])
+d=np.array([[[1,2,9],[3,4,10]],[[5,6,11],[7,8,12]]])
+
+for x in np.nditer(d):
+    print(x)
+
+print()
+
+for x in np.nditer(d[:,:,::2]):
+    print(x)
+
+print()
+
+for x in np.nditer(d[:,::2,::2]):
+    print(x)
+
+print()
+
+arr = np.array([1, 2, 3])
+for x in np.nditer(arr, flags=['buffered'], op_dtypes=['S']):
+  print(x)
