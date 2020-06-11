2cc0401e9a465c12eb7ad24f7e0d9bccca9bff60
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-11 11:11:14 +0530

    Create corona_cases.py

---
+++ Mathplotlib/corona_cases.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt 
+x=['1 MAR','15 MAR','30 MAR','15 APR','30 APR','15 MAY','30 MAY']
+y=[1,100,1800,10000,25000,53000,90000]
+plt.title("Corono cases in India")
+plt.xlabel("Date ")
+plt.ylabel("People")
+
+plt.plot(x,y,'b--')
+y=[0,2,32,420,1150,2750,5185]
+plt.plot(x,y,'r-')
+plt.show()
