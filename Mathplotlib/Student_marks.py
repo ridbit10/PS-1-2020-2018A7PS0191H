53a57e293c7550f9e61ee3289da43ee7ce75247f
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-11 10:55:03 +0530

    Create Student_marks.py

---
+++ Mathplotlib/Student_marks.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt 
+x=['Advait','Ridhiman','Rishabh','Sarthak','Shiv/Shankar']
+y=[70,45,75,80,65]
+plt.bar(x,y)
+plt.title("Marks in python")
+plt.xlabel("Student ")
+plt.ylabel("marks")
+
+plt.show()
