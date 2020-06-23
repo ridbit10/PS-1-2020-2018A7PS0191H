d87f933c316c479862ed1e65c46d39398b2ee487
ridbit10 <57602817+ridbit10@users.noreply.github.com>
2020-06-23 12:44:13 +0530

    Create basic.py

---
+++ Pandas/basic.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+player={"name":["messi","suarez","alba","ter stegen"],
+        "country":["argentina","uruguay","spain","germany"],
+        "jersey":["10","9","18","1"]
+        }
+
+plrs=pd.DataFrame(player)
+print(plrs)
